0a025e380cf66df1ab76e8464a362c9263014e96
nathan240802 <reisyan.nathan.k@gmail.com>
2023-01-26 23:36:45 +0700

    CreatingIdentityUser

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,15 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context,UserManager<AppUser> userManager)
         {
+
+            if(!userManager.Users.Any()){
+                var users= new List<AppUser>
+                {
+                    new AppUser {DisplayName="Reisyan" ,UserName="reisyan", Email="reisyan@gmail.com"},
+                    new AppUser {DisplayName="Nathan" ,UserName="nathan", Email="nathan@gmail.com"},
+                    new AppUser {DisplayName="Kusuma" ,UserName="kusuma", Email="kusuma@gmail.com"}
+                };
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user,"Pa$$w0rd");
+                }
+            }
+
             if (context.Activities.Any()) return;
             
             var activities = new List<Activity>
